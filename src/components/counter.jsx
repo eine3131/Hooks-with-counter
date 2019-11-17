f5d19154842aff023318067687a1098a59c15020
Navaloniaina Rakotovao <tsiresy+1@gmail.com>
2019-11-18 00:38:22 +0100

    modif count

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,23 +1,23 @@
 import React, { useState } from 'react';
 
 function Counter() {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(null);
 
 function handleSetCount(e) {
-    setCount(e.target.value)
+    setCount(Number(e.target.value));
 }
 
     return (
         <div>
             <p>
-                Le compteur est à : 
+                Le compteur est à : {count}
             </p>
             <input value={count} onChange={handleSetCount} />    
             <button 
-                onClick={() => setCount(count + 1)}>
+                onClick={() => setCount(count + 1)}> +1
             </button>    
             <button 
-                onclick={() => setCount(count - 1)}>
+                onclick={() => setCount(count - 1)}> -1
             </button>
         </div>
     );
