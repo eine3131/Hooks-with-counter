fae9029c092938e932383a4c5387b33ef3328c4a
Navaloniaina Rakotovao <tsiresy+1@gmail.com>
2019-11-17 23:12:52 +0100

    re commit cause nothing seen on netlify

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Counter from './components/counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <counter />
+      <Counter />
     </div>
   );
 }
